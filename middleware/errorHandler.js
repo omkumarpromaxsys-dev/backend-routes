711515792f9d420759c9e7525ad5756e0dba1bfb
Rahul Daivadnya <promaxsys.rahuldaivadnya@gmail.com>
2026-02-02 16:29:01 +0530

    protected routes for pg and room

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,14 @@
 const errorHandler = (err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send('Something broke!');
+  console.error(err);
+
+  res.status(err.status || 500).json({
+    success: false,
+    data: null,
+    error: {
+      code: err.code || "SERVER_ERROR",
+      message: err.message || "Something went wrong",
+    },
+  });
 };
 
 
--- routes/pg.routes.js
+++ routes/pg.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import {createPG,getAllPGs} from "../controllers/pg.controller.js"
-
+import { createPG, getAllPGs } from "../controllers/pg.controller.js";
+import { protect } from "../middleware/auth.js";
+import { requireRole } from "../middleware/role.js";
 const router = express.Router();
 
-
-router.post("/",createPG);
-router.get("/",getAllPGs);
+router.post("/", protect, requireRole("provider"), createPG);
+router.get("/", protect, requireRole("student"), getAllPGs);
 
 export default router;
