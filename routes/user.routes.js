c972d1a5565195e74d9c798b3975bd7b236b39df
Rahul Daivadnya <promaxsys.rahuldaivadnya@gmail.com>
2026-02-04 14:12:59 +0530

    more updates

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { getUser } from "../controllers/user.controller";
+import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
 
-router.get("/",getUser);
+router.get("/",protect,getUser);
 
 export default router;
